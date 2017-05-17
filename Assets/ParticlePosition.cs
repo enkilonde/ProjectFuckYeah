d25aa1386401af633bdc790ecb23e6fb8e9f3744
Farrand <rf.robert@yahoo.fr>
2017-05-17 09:50:06 +0200

    Removed shaderforge. Add simple particle system for dust(

---
+++ Assets/ParticlePosition.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlePosition : MonoBehaviour {
+
+	RaycastHit rayhit;
+
+	CharacterV3 cv3;
+
+	ParticleSystem partSys;
+
+	void Start () {
+		cv3 = transform.parent.GetComponent<CharacterV3>();
+		partSys = GetComponent<ParticleSystem>();
+	}
+	
+	void Update () {
+
+		if(cv3.currentAltitude > 5f)
+		{
+			partSys.Stop();
+		}
+		else
+		{
+			partSys.Play();
+		}
+
+		if(Physics.Raycast(transform.parent.position, -transform.parent.up, out rayhit, 2f))
+		{
+			transform.position = rayhit.point;
+		}
+
+	}
+}
